3ecef2c8fd5800d29519c9ca54889a56d7c47cc9
Stefan Dragnev <dragnev@telerik.com>
2013-11-21 12:08:48 +0200

    improved login; WIP project build * added logging * login now saves the session cookie to the user profile folder * added support for Fiddler * implemented command dispatch from the command line * fixed unit tests (were false positives)

--- test/server.js
+++ test/server.js
@@ -1,21 +1,26 @@
 "use strict";
 
-var assert = require("assert");
+var assert = require("chai").assert;
 
 describe("server", function() {
 	var server = require("../lib/server");
 
 	describe("basicLogin", function() {
-		it("should log in using valid telerik credentials", function() {
-			server.basicLogin("tailsu@gmail.com", "pDoVKoPCCRV", function(token) {
-				assert.isNotNull(token, "is valid token");
-				assert(token.length > 10, "is non-empty string");
+		it("should log in using valid telerik credentials", function(done) {
+			this.timeout(10000);
+			server.basicLogin("tailsu@gmail.com", "pDoVKoPCCRV4", function(token, response) {
+				assert.isString(token, "is valid token");
+				assert.equal(200, response.statusCode);
+				done();
 			})
 		})
 
-		it("should return error when using invalid credentials", function() {
-			server.basicLogin("nothing@example.org", "xxx", function(token) {
-				assert.isUndefined(token);
+		it("should return error when using invalid credentials", function(done) {
+			this.timeout(10000);
+			server.basicLogin("nothing@example.org", "xxx", function(token, response) {
+				assert.notOk(token);
+				assert.equal(401, response.statusCode);
+				done();
 			})
 		})
 	})
