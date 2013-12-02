8decd7eed4fdee17cc1fc435938f9cfbe049383e
Stefan Dragnev <dragnev@telerik.com>
2013-12-02 16:22:58 +0200

    browser-based login tunneled through Ice server

--- test/server.js
+++ test/server.js
@@ -8,18 +8,18 @@ describe("server", function() {
 	describe("basicLogin", function() {
 		it("should log in using valid telerik credentials", function(done) {
 			this.timeout(10000);
-			server.basicLogin("tailsu@gmail.com", "pDoVKoPCCRV4", function(token, response) {
+			server.basicLogin("tailsu@gmail.com", "pDoVKoPCCRV4", function(err, token) {
 				assert.isString(token, "is valid token");
-				assert.equal(200, response.statusCode);
+				assert.isNull(err);
 				done();
 			});
 		});
 
 		it("should return error when using invalid credentials", function(done) {
 			this.timeout(10000);
-			server.basicLogin("nothing@example.org", "xxx", function(token, response) {
+			server.basicLogin("nothing@example.org", "xxx", function(err, token) {
 				assert.notOk(token);
-				assert.equal(401, response.statusCode);
+				assert.isNotNull(err);
 				done();
 			});
 		});
