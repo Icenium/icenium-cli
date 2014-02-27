5db3d4026e6e590773ab494dd9be5a997c5d3c52
Stefan Dragnev <dragnev@telerik.com>
2014-02-27 16:26:35 +0200

    CLI with prebuilt dependencies

--- preinstall.js
+++ preinstall.js
@@ -1,29 +1,8 @@
-var readline = require("readline");
-
 if (process.platform !== "win32") {
 	return;
 }
-
-var rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
-});
 
 if (process.arch !== "ia32") {
 	console.log("Install the 32-bit version of Node.js before installing AppBuilder CLI.");
 	process.exit(1);
 }
-
-var info = [
-	"Before proceeding with the installation, review that you have the following installed:",
-	"",
-	"* Python 2.7.x - http://www.python.org/ftp/python/",
-	"* Visual Studio 2010/2012/2013 (with C++ compiler installed) or Windows 7 Platform SDK",
-	""
-];
-
-console.log(info.join("\n"));
-
-rl.question("Press ENTER to proceed with the installation.", function(answer) {
-	rl.close();
-});
