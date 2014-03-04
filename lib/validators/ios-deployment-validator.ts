7f603bb8561bb9d3bb1733ade1d5eb66aae23360
Fatme Havaluova <hfatme@gmail.com>
2014-03-04 14:30:53 +0200

    changes after code review

--- lib/validators/ios-deployment-validator.ts
+++ lib/validators/ios-deployment-validator.ts
@@ -11,14 +11,12 @@ import _ = require("underscore");
 import moment = require("moment");
 
 export class IOSDeploymentValidator extends BaseValidators.BaseAsyncValidator<IiOSDeploymentValidatorModel> {
-	private static NOT_SPECIFIER_PROVISION_ERROR_MESSAGE = "Please specify the provisioning profile (with option --provision) that should be used when building the project.";
-	private static MISSING_PROVISION_ERROR_MESSAGE = "No provisioning profile found.";
-	private static NOT_FOUND_PROVISION_ERROR_MESSAGE = "Could not find provision with name or starting with name '%s'. List registered provisions with 'list-provisions' command";
+	private static NOT_SPECIFIED_PROVISION_ERROR_MESSAGE = "Please specify the provisioning profile (with option --provision) that should be used when building the project.";
+	private static NOT_FOUND_PROVISION_ERROR_MESSAGE = "Could not find provision by specified index or name. List registered provisions with 'list-provisions' command";
 	private static EXPIRED_PROVISON_ERROR_MESSAGE = "Provisioning profile is expired.";
 	private static APPLICATION_IDENTIFIER_MISMATCH = "The application identifier must match the bundle identifier in the provisioning profile.";
-	private static NOT_SPECIFIER_CERTIFICATE_ERROR_MESSAGE = "Please specify the certificate (with option --certificate) that should be used when building the project.";
-	private static MISSING_CERTIFICATE_ERROR_MESSAGE = "Certificate is missing.";
-	private static NOT_FOUND_CERTIFICATE_ERROR_MESSAGE = "Could not find valid certificates for provision '%s'";
+	private static NOT_SPECIFIED_CERTIFICATE_ERROR_MESSAGE = "Please specify the certificate (with option --certificate) that should be used when building the project.";
+	private static NOT_FOUND_CERTIFICATE_ERROR_MESSAGE = "Could not find certificate by specified index or name. List registered certificates  with 'list-certificates' command";
 	private static EXPIRED_CERTIFICATE_ERROR_MESSAGE = "Certificate is expired.";
 
 	constructor(private appIdentifier: string,
@@ -31,11 +29,11 @@ export class IOSDeploymentValidator extends BaseValidators.BaseAsyncValidator<Ii
 	public validate(model: IiOSDeploymentValidatorModel): IFuture<IValidationResult> {
 		return (() => {
 			if(!model.provisionOption) {
-				return new ValidationResult.ValidationResult(IOSDeploymentValidator.NOT_SPECIFIER_PROVISION_ERROR_MESSAGE);
+				return new ValidationResult.ValidationResult(IOSDeploymentValidator.NOT_SPECIFIED_PROVISION_ERROR_MESSAGE);
 			}
 
 			if(!model.certificateOption) {
-				return new ValidationResult.ValidationResult(IOSDeploymentValidator.NOT_SPECIFIER_CERTIFICATE_ERROR_MESSAGE);
+				return new ValidationResult.ValidationResult(IOSDeploymentValidator.NOT_SPECIFIED_CERTIFICATE_ERROR_MESSAGE);
 			}
 
 			var provision = this.$identityManager.findProvision(model.provisionOption).wait();
@@ -60,9 +58,6 @@ export class IOSDeploymentValidator extends BaseValidators.BaseAsyncValidator<Ii
 	private validateProvision(provision: IProvision):IValidationResult {
 		if(!provision) {
 			return new ValidationResult.ValidationResult(IOSDeploymentValidator.NOT_FOUND_PROVISION_ERROR_MESSAGE);
-		}
-		if(provision === null) {
-			return new ValidationResult.ValidationResult(IOSDeploymentValidator.MISSING_PROVISION_ERROR_MESSAGE);
 		}
 		if(Date.parse(provision.ExpirationDate) < new Date().getTime()) {
 			return new ValidationResult.ValidationResult(IOSDeploymentValidator.EXPIRED_PROVISON_ERROR_MESSAGE);
@@ -90,14 +85,11 @@ export class IOSDeploymentValidator extends BaseValidators.BaseAsyncValidator<Ii
 				return new ValidationResult.ValidationResult(IOSDeploymentValidator.NOT_FOUND_CERTIFICATE_ERROR_MESSAGE);
 			}
 
-			if(certificate === null) {
-				return new ValidationResult.ValidationResult(IOSDeploymentValidator.MISSING_CERTIFICATE_ERROR_MESSAGE);
-			}
+			var formattedCertificate = helpers.stringReplaceAll(certificate.Certificate, "\r", "");
+			formattedCertificate = helpers.stringReplaceAll(formattedCertificate, "\n", "");
 
-			var formattedCertificate = helpers.stringReplaceAll(certificate.Certificate, "\r\n", "");
-
-			if(!_.some(provision.Certificates,(c) => { return formattedCertificate.indexOf(c) > 0; })) {
-				return new ValidationResult.ValidationResult(IOSDeploymentValidator.MISSING_CERTIFICATE_ERROR_MESSAGE);
+			if(!_.some(provision.Certificates,(c) => formattedCertificate.indexOf(c) >= 0 )) {
+				return new ValidationResult.ValidationResult("Certificate is not included in provision's certificates");
 			}
 
 			if(this.isCertificateExpired(certificate.Certificate)) {
@@ -119,8 +111,8 @@ export class IOSDeploymentValidator extends BaseValidators.BaseAsyncValidator<Ii
 		return moment(notAfter, "YYDDMMHHmmss").toDate() <= new Date();
 	}
 
-	private getRegexPattern(appIdentifier) {
-		var starPlaceholder = "\<!StarPlaceholder!>";
+	private getRegexPattern(appIdentifier): string {
+		var starPlaceholder = "<!StarPlaceholder!>";
 		var escapedIdentifier = (<any>RegExp).escape(helpers.stringReplaceAll(appIdentifier, "*", starPlaceholder));
 		var replacedIdentifier = helpers.stringReplaceAll(escapedIdentifier, starPlaceholder, ".*");
 		return "^" + replacedIdentifier + "$";
