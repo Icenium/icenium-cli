9f6eb1202031ea3769b02302e17309b2721caa03
Fatme Havaluova <Fatme.Havaluova@telerik.com>
2014-03-11 14:54:11 +0200

    ProductType for iOS device

--- lib/mobile/ios/ios-device.ts
+++ lib/mobile/ios/ios-device.ts
@@ -36,7 +36,7 @@ export class IOSDevice implements Mobile.IIOSDevice {
 	}
 
 	public getModel(): string {
-		return this.getValue("DeviceClass");
+		return this.getValue("ProductType");
 	}
 
 	public getVersion(): string {
@@ -49,10 +49,12 @@ export class IOSDevice implements Mobile.IIOSDevice {
 
 	private getValue(value: string): string {
 		this.connect();
+		this.startSession();
 		try {
 			var cfValue =  this.$coreFoundation.createCFString(value);
 			return this.$coreFoundation.convertCFStringToCString(this.$mobileDevice.deviceCopyValue(this.devicePointer, null, cfValue));
 		} finally {
+			this.stopSession();
 			this.disconnect();
 		}
 	}
