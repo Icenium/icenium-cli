a9abd58d35e826779ca9e4001ed7554ccc964a69
Stefan Dragnev <dragnev@telerik.com>
2014-04-22 11:57:52 +0300

    ensure all platform assets before running simulator

--- lib/.d.ts
+++ lib/.d.ts
@@ -87,6 +87,7 @@
 /// <reference path="services/cordova-plugins.ts" />
 /// <reference path="services/path-filtering.ts" />
 /// <reference path="services/server-extensions.ts" />
+/// <reference path="services/platform-migration.ts" />
 /// <reference path="services/user-settings-service.ts" />
 /// <reference path="templates-service.ts" />
 /// <reference path="validators/base-validators.ts" />
--- lib/commands/simulate.ts
+++ lib/commands/simulate.ts
@@ -20,6 +20,8 @@ export class SimulateCommand implements ICommand {
 		private $server: Server.IServer,
 		private $project: Project.IProject,
 		private $loginManager: ILoginManager,
+		private $platformServices: ISimulatorPlatformServices,
+		private $platformMigrator: Project.IPlatformMigrator,
 		private $simulatorPlatformServices: IExtensionPlatformServices,
 		private $serverExtensionsService: IServerExtensionsService) {
 		this.projectData = $project.projectData;
@@ -35,6 +37,7 @@ export class SimulateCommand implements ICommand {
 			this.simulatorPath = this.$serverExtensionsService.getExtensionPath(simulatorPackageName);
 			this.$serverExtensionsService.prepareExtension(simulatorPackageName).wait();
 			this.prepareCordovaPlugins(simulatorPackageName).wait();
+			this.$platformMigrator.ensureAllPlatformAssets().wait();
 
 			this.runSimulator();
 		}).future<void>()();
