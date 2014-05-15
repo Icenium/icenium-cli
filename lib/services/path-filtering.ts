2aad4d1b0327879f7aded9cbf495c260e22bdfde
Kristian Tashkov <kristian.tashkov@telerik.com>
2014-05-15 18:08:48 +0300

    Revert "Correctly trim project dir when filtering from .abignore"
    This reverts commit 4efee9e3d9189d1678d9d40ca40c7a97bff7c6a6.

--- lib/services/path-filtering.ts
+++ lib/services/path-filtering.ts
@@ -28,7 +28,7 @@ export class PathFilteringService implements IPathFilteringService {
 
 	public filterIgnoredFiles(files: string[], rules: string[], rootDir: string): string[]{
 		var selectedFiles = _.select(files, (file: string) => {
-			file = file.substr(rootDir.length + 1);
+			file = file.replace(rootDir, "");
 			var fileMatched = true;
 			_.forEach(rules, rule => {
 				// minimatch treats starting '!' as pattern negation
