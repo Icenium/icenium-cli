c2b16b8e15f5dd3ea41b905b0d7689ace240df90
Todor Totev <teobugslayer@gmail.com>
2014-05-19 15:26:14 +0300

    Fixes for cherry picking commits

--- lib/commands-service.ts
+++ lib/commands-service.ts
@@ -15,6 +15,7 @@ export class CommandsService implements ICommandsService {
 	}
 
 	constructor(private $errors: IErrors,
+		private $logger: ILogger,
 		private $injector: IInjector) { }
 
 	public allCommands(includeDev: boolean): string[] {
