df4f7ac7b5009a25cbbac22c25bb64a009f5d365
Stefan Dragnev <dragnev@telerik.com>
2014-08-06 10:53:33 +0300

    fix build

--- BuildPackage.cmd
+++ BuildPackage.cmd
@@ -8,4 +8,5 @@ set APPBUILDER_SKIP_POSTINSTALL_TASKS=
 call grunt.cmd pack --no-color
 
 call npm.cmd cache rm appbuilder
-cmd /C "rmdir /S /Q node_modules"
+call npm.cmd install -g rimraf
+call rimraf.cmd node_modules
