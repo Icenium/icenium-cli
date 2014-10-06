891cb021675167ea49c49c3dd154617fffe5910a
rosen-vladimirov <rosen.vladimirov@telerik.com>
2014-10-06 11:44:30 +0300

    Remove duplicate common options
    Common knownOpts are removed from options file. They are loaded from common/options.ts file, so there's no need to write them in two different places. Add tests for shorthands options and incorrect type of value - when the option is Boolean, passing string to it should cause process exit.

--- lib/options.ts
+++ lib/options.ts
@@ -1,43 +1,37 @@
 ///<reference path=".d.ts"/>
 "use strict";
 
-import path = require("path");
-import util = require("util");
-import osenv = require("osenv");
 import helpers = require("./common/helpers");
+import commonOptions = require("./common/options");
+import _ = require("underscore"); 
 var yargs: any = require("yargs");
 
 var knownOpts: any = {
-		"log": String,
-		"verbose": Boolean,
 		"companion": Boolean,
 		"download": Boolean,
 		"certificate": String,
 		"provision": String,
-		"path": String,
 		"template": String,
 		"appid": String,
 		"deploy": String,
-		"watch": Boolean,
 		"device": String,
 		"file": String,
 		"livesync": Boolean,
-		"version": Boolean,
-		"help": Boolean,
 		"timeout": String,
 		"save-to": String,
-		"json": Boolean,
 		"client": String
 	},
 	shorthands: IStringDictionary = {
-		"v": "verbose",
-		"p": "path",
 		"t": "template"
 	};
 
-var defaultProfileDir = path.join(osenv.home(), ".appbuilder-cli");
-var parsed = helpers.getParsedOptions(knownOpts, shorthands, defaultProfileDir);
 
+var parsed = yargs.argv;
+_.extend(knownOpts, commonOptions.knownOpts);
+_.extend(shorthands, commonOptions.shorthands);
+
+parsed = helpers.getParsedOptions(knownOpts, shorthands);
+parsed["profile-dir"] = commonOptions["profile-dir"];
 Object.keys(parsed).forEach((opt) => exports[opt] = parsed[opt]);
 
 exports.knownOpts = knownOpts;
