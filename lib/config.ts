a397091de960c12828883e43653b987b1ce313cb
Fatme <Fatme.Havaluova@telerik.com>
2015-01-21 08:17:57 +0200

    Revert "Share 7zip between CLIs"

--- lib/config.ts
+++ lib/config.ts
@@ -100,5 +100,9 @@ export class StaticConfig extends staticConfigBaseLib.StaticConfigBase implement
 	public get helpTextPath() {
 		return path.join(__dirname, "../resources/help.txt");
 	}
+
+	public get sevenZipFilePath() {
+		return path.join(__dirname, util.format("../resources/platform-tools/unzip/%s/7za", process.platform));
+	}
 }
 $injector.register("staticConfig", StaticConfig);
