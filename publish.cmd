bb18f3e886c2aaf486699f0dc0700941678b9e0b
Todor Totev <teobugslayer@gmail.com>
2015-04-29 15:35:31 +0300

    Change publish.cmd to also create a github tag

--- publish.cmd
+++ publish.cmd
@@ -1,1 +1,21 @@
+
+@if "%1" == "" goto error
+@if "%2" == "" goto error
+
+@for /f "delims=. tokens=1-3" %%a in ( "%2" ) do (set major=%%a && set minor=%%b && set revision=%%c)
+
+@if not defined revision goto error
+@if not defined minor goto error
+@if not defined major goto error
+
+git fetch
+git tag -a v%2 -m "Telerik AppBuilder %2" remotes/origin/master
+git push origin v%2
+
+@goto :EOF
+
+:error
+@echo Sample usage: publish appbuilder.tgz 1.2.3
+@echo Version string must be in Major.Minor.Revision format
+
 npm publish "%1" --ignore-scripts
