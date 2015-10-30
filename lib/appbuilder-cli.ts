507c7893c4f272d652d0a05c57762237d63841cc
Todor Totev <teobugslayer@gmail.com>
2015-10-30 16:52:58 +0200

    verify current node version against allowed list update common lib list of verified node versions
    See http://teampulse.telerik.com/view#item/304396

--- lib/appbuilder-cli.ts
+++ lib/appbuilder-cli.ts
@@ -1,6 +1,9 @@
 ///<reference path=".d.ts"/>
 "use strict";
 
+// this call must be first to avoid requiring c++ dependencies
+require("./common/verify-node-version").verifyNodeVersion(require("../package.json").engines.node);
+
 require("./bootstrap");
 import fiberBootstrap = require("./fiber-bootstrap");
 fiberBootstrap.run(() => {
