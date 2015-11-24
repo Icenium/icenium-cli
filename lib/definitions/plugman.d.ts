01fa5dbb133649eb5ac6c8b28154079c77b4461a
rosen-vladimirov <rosen.vladimirov@telerik.com>
2015-11-24 09:32:53 +0200

    Fix support for node 5.1.0 by updating plugman
    Fix support for node 5.1.0. Curently the installation is failling because we are using old plugman.
    This old plugman depends on very old version of npm. During installation with npm 3.3.11 or later, the plugman fails.
    This is a bug in npm itself (https://github.com/npm/npm/issues/10234). However updating our version of plugman is fixing the installation.
    The new plugman had change the callback parameters - previously it was one parameter, now it has two - error and result.
    Fix our callbacks to have two arguments.

--- lib/definitions/plugman.d.ts
+++ lib/definitions/plugman.d.ts
@@ -1,5 +1,5 @@
 declare module "plugman" {
-	function config(params: string[], callback: (result: any) => void): void;
-	function fetch(plugin_dir:string, plugins_dir:string, link:boolean, subdir:string, git_ref:string, callback: (result: any) => void): void;
-	function search(keywords: string[], callback: (result: any) => void): void;
+	function config(params: string[], callback: (error: Error, result: any) => void): void;
+	function fetch(plugin_dir:string, plugins_dir:string, link:boolean, subdir:string, git_ref:string, callback: (error: Error, result: any) => void): void;
+	function search(keywords: string[], callback: (error: Error, result: any) => void): void;
 }
