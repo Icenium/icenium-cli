1bccbf75fc3956ad2c456a141285c16c499b08e3
TsvetanMilanov <tsvetan.milanov17@gmail.com>
2016-03-24 14:02:49 +0200

    Fix bug in commands fail if executed outside project.
    Created base ensure project command class to extract the common can execute method which checks if there is project when the command depends on project.
    Refactored the other similar commands to remove the repeating can execute method.

---
+++ lib/commands/ensure-project-command.ts
@@ -0,0 +1,28 @@
+"use strict";
+
+import Future = require("fibers/future");
+import assert = require("assert");
+
+export class EnsureProjectCommand implements ICommand {
+	constructor(protected $project: Project.IProject,
+		protected $errors: IErrors) { }
+
+	allowedParameters: ICommandParameter[] = [];
+
+	execute(args: string[]): IFuture<void> {
+		assert.fail("","", "You should never get here. Please contact Telerik support and send the output of your command, executed with `--log trace`.");
+		return Future.fromResult();
+	}
+
+	canExecute(args: string[]): IFuture<boolean> {
+		return (() => {
+			if(args.length) {
+  				this.$errors.fail("This command doesn't accept parameters.");
+				return false;
+  			}
+
+			this.$project.ensureProject();
+			return true;
+		}).future<boolean>()();
+	}
+}
