cb0b198ce0a65554040fb65ae157312d9d411ddc
TsvetanMilanov <tsvetan.milanov17@gmail.com>
2016-08-19 17:47:32 +0300

    Fix PR comments

--- lib/hooks/typescript-compilation.ts
+++ lib/hooks/typescript-compilation.ts
@@ -4,32 +4,34 @@ import fiberBootstrap = require("./../common/fiber-bootstrap");
 import { TARGET_FRAMEWORK_IDENTIFIERS } from "../common/constants";
 
 fiberBootstrap.run(() => {
-	let project: Project.IProject = $injector.resolve("project");
-	if (!project.projectData) {
+	let $project: Project.IProject = $injector.resolve("project");
+	if (!$project.projectData) {
 		return;
 	}
 
-	let typeScriptService: ITypeScriptService = $injector.resolve("typeScriptService");
-	let typeScriptFiles = typeScriptService.getTypeScriptFiles(project.getProjectDir().wait()).wait();
+	let $projectConstants: Project.IConstants = $injector.resolve("projectConstants");
+	let $typeScriptService: ITypeScriptService = $injector.resolve("typeScriptService");
+	let typeScriptFiles = $typeScriptService.getTypeScriptFiles($project.getProjectDir().wait()).wait();
 
-	if (typeScriptFiles.typeScriptFiles.length > typeScriptFiles.definitionFiles.length) { // We need this check because some of non-typescript templates(for example KendoUI.Strip) contain typescript definition files
+	if ($typeScriptService.isTypeScriptProject($project.projectDir).wait()) {
 		let $fs: IFileSystem = $injector.resolve("fs");
-		let pathToTsConfig = path.join(project.getProjectDir().wait(), "tsconfig.json");
+		let pathToTsConfig = path.join($project.projectDir, $projectConstants.TSCONFIG_JSON_NAME);
 
-		if (project.projectData.Framework.toLowerCase() === TARGET_FRAMEWORK_IDENTIFIERS.NativeScript.toLowerCase()) {
+		if ($project.projectData.Framework.toLowerCase() === TARGET_FRAMEWORK_IDENTIFIERS.NativeScript.toLowerCase()) {
 			let $projectMigrationService: Project.IProjectMigrationService = $injector.resolve("projectMigrationService");
 			$projectMigrationService.migrateTypeScriptProject().wait();
 			let $npmService: INpmService = $injector.resolve("npmService");
-			$npmService.install(project.projectDir).wait();
+			$npmService.install($project.projectDir).wait();
 		}
 
 		if ($fs.exists(pathToTsConfig).wait()) {
 			let json = $fs.readJson(pathToTsConfig).wait();
 			let noEmitOnError = !!(json && json.compilerOptions && json.compilerOptions.noEmitOnError);
-			typeScriptService.transpileWithDefaultOptions(project.getProjectDir().wait(), { compilerOptions: { noEmitOnError } }).wait();
+			$typeScriptService.transpile($project.getProjectDir().wait(), null, null, { compilerOptions: { noEmitOnError } }).wait();
 		} else {
-			let pathToDefaultDefinitionFiles = path.join(__dirname, "../../resources/typescript-definitions-files");
-			typeScriptService.transpile(project.getProjectDir().wait(), typeScriptFiles.typeScriptFiles, typeScriptFiles.definitionFiles, { compilerOptions: { noEmitOnError: false }, pathToDefaultDefinitionFiles }).wait();
+			let $resources: IResourceLoader = $injector.resolve("resources");
+			let pathToDefaultDefinitionFiles = $resources.resolvePath(path.join("resources", "typescript-definitions-files"));
+			$typeScriptService.transpile($project.projectDir, typeScriptFiles.typeScriptFiles, typeScriptFiles.definitionFiles, { compilerOptions: { noEmitOnError: false }, pathToDefaultDefinitionFiles }).wait();
 		}
 	}
 });
