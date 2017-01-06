9fe599381f6a619ee0919e11aedd7bc12eed2293
rosen-vladimirov <rosen.vladimirov@telerik.com>
2017-01-06 12:56:26 +0200

    Revert incorrectly commented code

--- lib/appbuilder-cli.ts
+++ lib/appbuilder-cli.ts
@@ -7,12 +7,12 @@ installUncaughtExceptionListener(process.exit);
 (async () => {
 	let commandDispatcher: ICommandDispatcher = $injector.resolve("commandDispatcher");
 
-	// let config: Config.IConfig = $injector.resolve("$config");
-	// let errors: IErrors = $injector.resolve("$errors");
-	// errors.printCallStack = config.DEBUG;
+	let config: Config.IConfig = $injector.resolve("$config");
+	let errors: IErrors = $injector.resolve("$errors");
+	errors.printCallStack = config.DEBUG;
 
-	// let messages = <IMessagesService>$injector.resolve("$messagesService");
-	// messages.pathsToMessageJsonFiles = [/* Place client-specific json message file paths here */];
+	let messages = <IMessagesService>$injector.resolve("$messagesService");
+	messages.pathsToMessageJsonFiles = [/* Place client-specific json message file paths here */];
 
 	if (process.argv[2] === "completion") {
 		await commandDispatcher.completeCommand();
