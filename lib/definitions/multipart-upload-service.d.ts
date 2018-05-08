89b4d3e841c7f6fa48b728e62fc0868d560e623e
rosen-vladimirov <rosen.vladimirov@progress.com>
2018-05-08 15:32:43 +0300

    Remove all unneeded files

--- lib/definitions/multipart-upload-service.d.ts
+++
@@ -1,9 +0,0 @@
-interface IMultipartUploadService {
-	/**
-	 * Upload file by chunks (multiple parts instead of single one).
-	 *
-	 * @param projectZipFile Full path to the zipped project.
-	 * @param bucketKey Key that will be used for identifying the parts of the upload. Each chunk must use the same bucketKey.
-	 */
-	uploadFileByChunks(projectZipFile: string, bucketKey: string): Promise<void>;
-}
--- prepublish.js
+++ prepublish.js
@@ -5,11 +5,10 @@ if (skipPostinstallTasks) {
 
 var fs = require("fs"),
 	path = require("path"),
-	child_process = require("child_process"),
-	nodeArgs = require("./lib/common/scripts/node-args").getNodeArgs();
+	child_process = require("child_process");
 
 function invokeGrunt(callback) {
-	if (!fs.existsSync("lib/appbuilder-cli.js")) {
+	if (!fs.existsSync("lib/version-validator.js")) {
 		var grunt = require("grunt");
 		grunt.cli.tasks = ["ts:devall"];
 		grunt.cli(null, callback);
@@ -17,16 +16,3 @@ function invokeGrunt(callback) {
 		process.nextTick(callback);
 	}
 }
-
-var commandArgs = ["bin/appbuilder", "dev-prepackage"];
-
-invokeGrunt(function () {
-	var child = child_process.exec("node " + nodeArgs.concat(commandArgs).join(" "), { env: process.env }, function (error) {
-		if (error) {
-			console.error("Failed to complete all pre-publishing steps.");
-			throw error;
-		}
-	});
-
-	child.stdout.pipe(process.stdout);
-});
